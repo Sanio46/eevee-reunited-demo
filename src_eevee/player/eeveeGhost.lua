3107fa1cccc858a3de554016f4fb96538bdc1aed
Sanio <BRude602@gmail.com>
2021-12-09 16:44:29 -0700

    Fixed Eevee's Ghost playing the incorrect default Death animation

--- src_eevee/player/eeveeGhost.lua
+++ src_eevee/player/eeveeGhost.lua
@@ -5,7 +5,14 @@ function eeveeGhost:SpawnGhostEffect(player)
 	local spritePlayer = player:GetSprite()
 	local dataPlayer = player:GetData()
 	
-	if playerType == EEVEEMOD.PlayerType.EEVEE and player:IsDead() then
+	if playerType == EEVEEMOD.PlayerType.EEVEE
+	and player:IsDead()
+	and (
+	spritePlayer:IsPlaying("Death")
+	or spritePlayer:IsPlaying("LostDeath")
+	or spritePlayer:IsPlaying("HoleDeath")
+	)
+	then
 		if not dataPlayer.EeveeGhostSpawned then
 			local ghostEffect = Isaac.Spawn(EntityType.ENTITY_EFFECT, EEVEEMOD.EffectVariant.EEVEE_GHOST, 0, player.Position, Vector.Zero, nil)
 			ghostEffect:GetSprite():Play(spritePlayer:GetAnimation(), true)
