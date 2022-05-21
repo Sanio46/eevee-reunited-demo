3413fc4be6b574bef164295544034b8a245251f2
Sanio <BRude602@gmail.com>
2022-05-21 16:38:39 -0700

    Small list of bug fixes to get around to

--- main.lua
+++ main.lua
@@ -3,6 +3,9 @@
 --TODO:
 --Black Candle edited ver for Eevee
 --Detect golden trinkets that drop for unlock manager
+--Isaac's Tears charing? I guess? lol.
+--Figure out what to do for postTearSplash and spectral tears on collision.
+--Spirit Sword still breaks when transitions rooms apparently
 
 local json = require("json")
 
