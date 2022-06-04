0aa1f2c4f98ceb3c4c0b4051320c5bc24a68cec7
Sanio <BRude602@gmail.com>
2022-06-03 20:42:30 -0700

    Swift 2.0 update, good progress

--- src_eevee/attacks/eevee/swiftAttack2.lua
+++ src_eevee/attacks/eevee/swiftAttack2.lua
@@ -2,25 +2,10 @@ local swift = {}
 
 local mod = RegisterMod("Swift Tear Testing", 1)
 
----@class SwiftWeapon
----@field Trail EntityEffect
----@field WeaponEntity Weapon
-local SwiftWeaponData = {
-	WeaponEntity = nil,
-	WeaponIndex = 0,
-	StartingPosition = Vector.Zero,
-	ShootDirection = Vector.Zero,
-	StartingAccel = 0,
-	OrbitDistance = 0,
-	Trail = nil,
-	FireDelay = 2,
-	HasFired = false,
-}
-
 ---@class SwiftInstance
 ---@field Player EntityPlayer
 ---@field Parent Entity
----@field ActiveWeapons SwiftWeapon[]
+---@field ActiveWeapons Weapon[]
 local swiftInstanceData = {
 	Player = nil,
 	Parent = nil,
@@ -36,9 +21,26 @@ local swiftInstanceData = {
 	NumWeaponsDead = 0,
 }
 
+---@class SwiftWeapon
+---@field Trail EntityEffect
+---@field WeaponEntity Weapon
+---@field ParentInstance SwiftInstance
+local SwiftWeaponData = {
+	ParentInstance = {},
+	StartingPosition = Vector.Zero,
+	ShootDirection = Vector.Zero,
+	StartingAccel = 0,
+	OrbitDistance = 0,
+	Trail = nil,
+	FireDelay = 2,
+	HasFired = false,
+}
+
 ---@type SwiftInstance[]
 local swiftInstances = {}
---Probably the new swiftBase
+---@type SwiftWeapon[]
+local swiftWeapons = {}
+
 -------------------------------
 --  SETTING / OBTANING DATA  --
 -------------------------------
@@ -61,7 +63,7 @@ end
 ---@param player EntityPlayer
 function swift:GetInstanceCooldown(player)
 	local fireDelay = swift:GetFireDelay(player) * 6
-	return fireDelay + math.abs(fireDelay)
+	return fireDelay
 end
 
 ---@param player EntityPlayer
@@ -86,66 +88,7 @@ end
 
 ---@param swiftData SwiftInstance
 function swift:GetStartingPos(swiftData)
-	local angle = (360 / swiftData.NumWeaponsToSpawn) * swiftData.NumWeaponsSpawned
-	return Vector.FromAngle(angle):Resized(swift:SwiftOrbitDistance(swiftData.Player))
-end
-
-function swift:WeaponToInstanceWeapon(swiftWeapon, weapon)
-	local isCorrectWeapon = false
-	if tostring(GetPtrHash(swiftWeapon)) == tostring(GetPtrHash(weapon)) then
-		isCorrectWeapon = true
-	end
-	return isCorrectWeapon
-end
-
--------------------
---  DOING STUFF  --
--------------------
-
-function swift:PlaySwiftFire()
-	local values = {
-		0.9,
-		1,
-		1.1
-	}
-	EEVEEMOD.sfx:Stop(SoundEffect.SOUND_TEARS_FIRE)
-	EEVEEMOD.sfx:Play(EEVEEMOD.SoundEffect.SWIFT_FIRE, 1, 2, false, values[EEVEEMOD.RandomNum(3)])
-end
-
----@param swiftData SwiftInstance
----@param player EntityPlayer
----@param parent Entity
-function swift:FireSwift(swiftData, player, parent)
-	local startPos = swift:GetStartingPos(swiftData)
-
-	local parentPos = parent:ToPlayer() and (player.Position - player.TearsOffset) or parent.Position
-	---@type EntityTear
-	local swiftTear = player:FireTear(parentPos + startPos:Rotated(swiftData.Rotation), Vector.Zero):ToTear()
-	swift:InitSwiftWeapon(swiftData, swiftTear)
-	---@type SwiftWeapon
-	local swiftWeapon = swiftData.ActiveWeapons[tostring(GetPtrHash(swiftTear))]
-	swiftWeapon.StartingPosition = startPos
-	swiftWeapon.StartingAccel = swiftTear.Height
-	swiftWeapon.OrbitDistance = swift:SwiftOrbitDistance(player)
-end
-
----@param swiftData SwiftInstance
----@param index integer
-function swift:RemoveSwiftInstance(swiftData, index)
-	if swiftData.CanFire and swiftData.NumWeaponsDead >= swiftData.NumWeaponsSpawned or swiftData.NumWeaponsDead >= swiftData.NumWeaponsToSpawn then
-		table.remove(swiftInstances, index)
-	end
-end
-
----@param weapon Weapon
-function swift:RemoveWeaponOnDeath(weapon)
-	for _, swiftData in ipairs(swiftInstances) do
-		local swiftWeapon = swiftData.ActiveWeapons[tostring(GetPtrHash(weapon))]
-
-		if swiftWeapon ~= nil then
-			swiftData.NumWeaponsDead = swiftData.NumWeaponsDead + 1
-		end
-	end
+	return Vector.FromAngle((360 / swiftData.NumWeaponsToSpawn) * swiftData.NumWeaponsSpawned):Resized(swift:SwiftOrbitDistance(swiftData.Player))
 end
 
 ------------------
@@ -163,7 +106,7 @@ function swift:InitInstance(player)
 	end
 	if data.TimeTillNextInstance then
 		if data.TimeTillNextInstance > 0 then
-			data.TimeTillNextInstance = data.TimeTillNextInstance - 1
+			data.TimeTillNextInstance = data.TimeTillNextInstance - 0.5
 		else
 			data.TimeTillNextInstance = nil
 		end
@@ -172,11 +115,11 @@ end
 
 ---@param player EntityPlayer
 ---@param parent Entity
-function swift:CreateSwiftInstance(player, parent, customData)
+function swift:CreateSwiftInstance(player, parent)
 	---@type SwiftInstance
 	local instance = {}
 	for variableName, value in pairs(swiftInstanceData) do
-		instance[variableName] = customData ~= nil and customData[variableName] or value
+		instance[variableName] =  value
 	end
 	instance.Player = player
 	instance.Parent = parent
@@ -188,15 +131,15 @@ end
 ---@param swiftData SwiftInstance
 ---@param weapon Weapon
 function swift:InitSwiftWeapon(swiftData, weapon)
+	table.insert(swiftData.ActiveWeapons, weapon)
 	local ptrHashWeapon = tostring(GetPtrHash(weapon))
-	if swiftData.ActiveWeapons[ptrHashWeapon] == nil then
-		swiftData.ActiveWeapons[ptrHashWeapon] = {}
-		local swiftWeapon = swiftData.ActiveWeapons[ptrHashWeapon]
+	if swiftWeapons[ptrHashWeapon] == nil then
+		swiftWeapons[ptrHashWeapon] = {}
+		local swiftWeapon = swiftWeapons[ptrHashWeapon]
 		for variableName, value in pairs(SwiftWeaponData) do
 			swiftWeapon[variableName] = value
 		end
-		swiftWeapon.WeaponEntity = weapon
-		swiftWeapon.WeaponIndex = swiftData.NumWeaponsSpawned
+		swiftWeapon.ParentInstance = swiftData
 	end
 end
 
@@ -204,9 +147,9 @@ end
 function swift:InitInstanceValues(swiftData)
 	local fireDelay = swift:GetFireDelay(swiftData.Player)
 	local totalDuration = fireDelay * swiftData.NumWeaponsToSpawn
-	swiftData.TotalDuration = swiftData.TotalDuration ~= 0 and swiftData.TotalDuration or totalDuration + 0.5
-	swiftData.DurationTimer = swiftData.DurationTimer ~= 0 and swiftData.DurationTimer or totalDuration + 0.5
-	swiftData.WeaponSpawnTimer = swiftData.WeaponSpawnTimer ~= 0 and swiftData.WeaponSpawnTimer or fireDelay
+	swiftData.TotalDuration = totalDuration + 0.5
+	swiftData.DurationTimer = totalDuration + 0.5
+	swiftData.WeaponSpawnTimer = fireDelay
 end
 
 ---@param weapon Weapon
@@ -273,6 +216,57 @@ function swift:MakeStarOnTearInit(tear)
 	end
 end
 
+-------------------
+--  DOING STUFF  --
+-------------------
+
+function swift:PlaySwiftFire()
+	local values = {
+		0.9,
+		1,
+		1.1
+	}
+	EEVEEMOD.sfx:Stop(SoundEffect.SOUND_TEARS_FIRE)
+	EEVEEMOD.sfx:Play(EEVEEMOD.SoundEffect.SWIFT_FIRE, 1, 2, false, values[EEVEEMOD.RandomNum(3)])
+end
+
+---@param swiftData SwiftInstance
+---@param player EntityPlayer
+---@param parent Entity
+function swift:FireSwift(swiftData, player, parent)
+	local startPos = swift:GetStartingPos(swiftData)
+
+	local parentPos = parent:ToPlayer() and (player.Position - player.TearsOffset) or parent.Position
+	---@type EntityTear
+	local swiftTear = player:FireTear(parentPos + startPos:Rotated(swiftData.Rotation), Vector.Zero):ToTear()
+	swift:InitSwiftWeapon(swiftData, swiftTear)
+	---@type SwiftWeapon
+	local swiftWeapon = swiftWeapons[tostring(GetPtrHash(swiftTear))]
+	swiftWeapon.StartingPosition = startPos
+	swiftWeapon.StartingAccel = swiftTear.Height
+	swiftWeapon.OrbitDistance = swift:SwiftOrbitDistance(player)
+end
+
+---@param swiftData SwiftInstance
+---@param index integer
+function swift:RemoveSwiftInstance(swiftData, index)
+	if swiftData.CanFire and swiftData.NumWeaponsDead >= swiftData.NumWeaponsSpawned or swiftData.NumWeaponsDead >= swiftData.NumWeaponsToSpawn then
+		table.remove(swiftInstances, index)
+	end
+end
+
+---@param weapon Weapon
+function swift:RemoveWeaponOnDeath(weapon)
+	local swiftWeapon = swiftWeapons[tostring(GetPtrHash(weapon))]
+	if swiftWeapon == nil then return end
+	local swiftData = swiftWeapon.ParentInstance
+	if swiftData == nil then return end
+
+	if swiftWeapon ~= nil then
+		swiftData.NumWeaponsDead = swiftData.NumWeaponsDead + 1
+	end
+end
+
 --------------------
 --  SWIFT UPDATE  --
 --------------------
@@ -291,7 +285,7 @@ function swift:SpawnNextWeapon(swiftData)
 	if swiftData.WeaponSpawnTimer > 0 then
 		swiftData.WeaponSpawnTimer = swiftData.WeaponSpawnTimer - 0.5
 	elseif swiftData.NumWeaponsSpawned < swiftData.NumWeaponsToSpawn then
-		swiftData.NumWeaponsSpawned = swiftData.NumWeaponsSpawned + 0.5
+		swiftData.NumWeaponsSpawned = swiftData.NumWeaponsSpawned + 1
 		swift:FireSwift(swiftData, swiftData.Player, swiftData.Parent)
 		swiftData.WeaponSpawnTimer = swift:GetFireDelay(swiftData.Player)
 	end
@@ -323,31 +317,31 @@ function swift:OnPostPlayerUpdate(player)
 		swift:RemoveSwiftInstance(swiftData, index)
 		swift:FireIfNotShooting(swiftData)
 		swift:RateOfOrbitRotation(swiftData, player)
-		if swiftData.CanFire then return end
-		swift:SpawnNextWeapon(swiftData)
-		swift:CountdownDuration(swiftData)
+		if not swiftData.CanFire then
+			swift:SpawnNextWeapon(swiftData)
+			swift:CountdownDuration(swiftData)
+		end
 	end
 end
 
 ---@param tear EntityTear
 function swift:OnPostTearUpdate(tear)
-	for _, swiftData in ipairs(swiftInstances) do
-		---@type SwiftWeapon
-		local swiftWeapon = swiftData.ActiveWeapons[tostring(GetPtrHash(tear))]
-		if swiftWeapon ~= nil then
-			if swiftData.CanFire == false then
-				tear.Velocity = swiftData.Parent.Position - (tear.Position - swiftWeapon.StartingPosition:Resized(swiftWeapon.OrbitDistance):Rotated(swiftData.Rotation))
-			end
-			if swiftWeapon.FireDelay > 0 then
-				if swiftData.CanFire == true then
-					swiftWeapon.FireDelay = swiftWeapon.FireDelay - 1
-				end
-				tear.Height = swiftWeapon.StartingAccel
-			elseif not swiftWeapon.HasFired then
-				swiftWeapon.HasFired = true
-				tear.Velocity = Vector(10, 0)
-			end
+	local swiftWeapon = swiftWeapons[tostring(GetPtrHash(tear))]
+	if swiftWeapon == nil then return end
+	local swiftData = swiftWeapon.ParentInstance
+	if swiftData == nil then return end
+
+	if swiftData.CanFire == false then
+		tear.Velocity = swiftData.Parent.Position - (tear.Position - swiftWeapon.StartingPosition:Resized(swiftWeapon.OrbitDistance):Rotated(swiftData.Rotation))
+	end
+	if swiftWeapon.FireDelay > 0 then
+		if swiftData.CanFire == true then
+			swiftWeapon.FireDelay = swiftWeapon.FireDelay - 1
 		end
+		tear.Height = swiftWeapon.StartingAccel
+	elseif not swiftWeapon.HasFired then
+		swiftWeapon.HasFired = true
+		tear.Velocity = Vector(10, 0)
 	end
 end
 
