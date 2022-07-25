2d6b5f393f9653c9418b15fdd9fe209116954478
Sanio <BRude602@gmail.com>
2022-07-25 10:55:39 -0700

    Fix incorrect rendering positions for all players Fix crash for starting a new game with a valid active item

--- src_eevee/items/activeItemRender.lua
+++ src_eevee/items/activeItemRender.lua
@@ -4,21 +4,21 @@ local function GetScreenBottomRight()
 	local hudOffset = Options.HUDOffset
 	local offset = Vector(-hudOffset * 16, -hudOffset * 6)
 
-	return Vector(Isaac.GetScreenWidth(), -1 * Isaac.GetScreenHeight()) + offset
+	return Vector(Isaac.GetScreenWidth(), Isaac.GetScreenHeight()) + offset
 end
 
 local function GetScreenBottomLeft()
 	local hudOffset = Options.HUDOffset
-	local offset = Vector(hudOffset * 20, -hudOffset * 12)
+	local offset = Vector(hudOffset * 22, -hudOffset * 6)
 
-	return Vector(0, -1 * Isaac.GetScreenHeight()) + offset
+	return Vector(0, Isaac.GetScreenHeight()) + offset
 end
 
 local function GetScreenTopRight()
 	local hudOffset = Options.HUDOffset
-	local offset = Vector(-hudOffset * 20, hudOffset * 12)
+	local offset = Vector(-hudOffset * 24, hudOffset * 12)
 
-	return Vector(Isaac.GetScreenWidth(), Isaac.GetScreenHeight()) + offset
+	return Vector(Isaac.GetScreenWidth(), 0) + offset
 end
 
 local function GetScreenTopLeft()
@@ -45,16 +45,16 @@ local IndexedPlayers = {
 		Player = nil,
 		ScreenPos = function() return GetScreenTopRight() end,
 		Offset = {
-			[ActiveSlot.SLOT_PRIMARY] = Vector(-159, 0),
-			[ActiveSlot.SLOT_SECONDARY] = Vector(-168, 0),
+			[ActiveSlot.SLOT_PRIMARY] = Vector(-155, 0),
+			[ActiveSlot.SLOT_SECONDARY] = Vector(-164, 0),
 		}
 	},
 	[3] = {
 		Player = nil,
 		ScreenPos = function() return GetScreenBottomLeft() end,
 		Offset = {
-			[ActiveSlot.SLOT_PRIMARY] = Vector(16, -33),
-			[ActiveSlot.SLOT_SECONDARY] = Vector(7, -33)
+			[ActiveSlot.SLOT_PRIMARY] = Vector(14, -39),
+			[ActiveSlot.SLOT_SECONDARY] = Vector(5, -39)
 		}
 	},
 	[4] = {
@@ -66,6 +66,14 @@ local IndexedPlayers = {
 		ScreenPos = function() return GetScreenBottomRight() end,
 	}
 }
+
+---@param player EntityPlayer
+local function GetCookieFrame(player)
+	local numCookieEffects = player:GetEffects():GetCollectibleEffectNum(EEVEEMOD.CollectibleType.COOKIE_JAR)
+	local cookieFrame = 5 - numCookieEffects
+	if cookieFrame < 0 then cookieFrame = 0 end
+	return cookieFrame
+end
 
 ---@type table<CollectibleType, {Sprite: Sprite, Directory: string, StartFrame: integer, UpdatedFrame: function, Condition?: function}>
 local activesToRender = {
@@ -87,13 +95,6 @@ local activesToRender = {
 	}
 }
 
----@param player EntityPlayer
-function GetCookieFrame(player)
-	local numCookieEffects = player:GetEffects():GetCollectibleEffectNum(EEVEEMOD.CollectibleType.COOKIE_JAR)
-	local cookieFrame = 5 - numCookieEffects
-	if cookieFrame < 0 then cookieFrame = 0 end
-	return cookieFrame
-end
 
 ---@param i integer
 ---@param player EntityPlayer
@@ -141,7 +142,7 @@ end
 function activeItemRender:RenderActiveItem()
 	for i = 1, #IndexedPlayers do
 		local activeItemPlayer = IndexedPlayers[i]
-		
+
 		if hasLoadedItems == false then
 			LoadItemSprites()
 			hasLoadedItems = true
@@ -156,7 +157,8 @@ function activeItemRender:RenderActiveItem()
 					local slots = VeeHelper.GetActiveSlots(player, itemID)
 					for k = 1, #slots do
 						if ((params.Condition == nil) or (params.Condition ~= nil and params.Condition(player, slots[k]) == true)) then
-							local pos = activeItemPlayer.ScreenPos() + (activeItemPlayer.Offset[slots[k]] or activeItemPlayer.Offset[slots[0]])
+							local pos = activeItemPlayer.ScreenPos() +
+								(activeItemPlayer.Offset[slots[k]] or activeItemPlayer.Offset[slots[0]])
 							local size = slots[k] == ActiveSlot.SLOT_PRIMARY and 1 or 0.5
 							local bookOffset = slots[k] == ActiveSlot.SLOT_PRIMARY and VeeHelper.GetBookState(player) > 0 and -4 or 0
 							params.Sprite:SetFrame(params.UpdatedFrame(player))
